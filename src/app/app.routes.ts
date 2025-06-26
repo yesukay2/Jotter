13ae8f3a8f7bdb6adc7341821102f1dd44af2a5b
Yesukay <yesukay2@gmail.com>
2025-06-26 14:23:30 -0400

    Make user profile pic and presets menu display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-// import { JotterListPageComponent } from './Pages/jotter-list-page/jotter-list-page.component';
-import { JotterPageComponent } from './Pages/jotter-page/jotter-page.component';
-import { JotterFormPageComponent } from './Pages/jotter-form-page/jotter-form-page.component';
-
 import { routeGuardGuard } from './Guard/route-guard.guard';
+import { JotterFormPageComponent } from './Pages/jotter-form-page/jotter-form-page.component';
+import { JotterListPageComponent } from './Pages/jotter-list-page/jotter-list-page.component';
+import { JotterPageComponent } from './Pages/jotter-page/jotter-page.component';
 import { ArchivedPageComponent } from './Pages/archived-page/archived-page.component';
 import { ConfirmDeleteComponent } from './Pages/confirm-delete/confirm-delete.component';
 
@@ -17,50 +16,33 @@ export const routes: Routes = [
   },
   {
     path: 'jotters',
-    loadComponent: () =>
-      import('./Pages/jotter-list-page/jotter-list-page.component').then(
-        (module) => module.JotterListPageComponent
-      ),
-    canActivate: [routeGuardGuard],
+    component: JotterListPageComponent,
+    // canActivate: [routeGuardGuard],
   },
   {
     path: 'jotter/view/:id',
-    loadComponent: () =>
-      import('./Pages/jotter-page/jotter-page.component').then(
-        (module) => module.JotterPageComponent
-      ),
-    canActivate: [routeGuardGuard],
+    component: JotterPageComponent,
+    // canActivate: [routeGuardGuard],
   },
   {
     path: 'jotter/new-jotter',
-    loadComponent: () =>
-      import('./Pages/jotter-form-page/jotter-form-page.component').then(
-        (module) => module.JotterFormPageComponent
-      ),
-    canActivate: [routeGuardGuard],
+    component: JotterFormPageComponent,
+    // canActivate: [routeGuardGuard],
   },
+
   {
     path: 'jotter/edit-jotter/:id',
-    loadComponent: () =>
-      import('./Pages/jotter-form-page/jotter-form-page.component').then(
-        (module) => module.JotterFormPageComponent
-      ),
-    canActivate: [routeGuardGuard],
+    component: JotterFormPageComponent,
+    // canActivate: [routeGuardGuard],
   },
   {
     path: 'archived',
-    loadComponent: () =>
-      import('./Pages/archived-page/archived-page.component').then(
-        (module) => module.ArchivedPageComponent
-      ),
-    canActivate: [routeGuardGuard],
+    component: ArchivedPageComponent,
+    // canActivate: [routeGuardGuard],
   },
   {
     path: 'jotter/edit-jotter/confirm-delete/:id',
-    loadComponent: () =>
-      import('./Pages/confirm-delete/confirm-delete.component').then(
-        (module) => module.ConfirmDeleteComponent
-      ),
+    component: ConfirmDeleteComponent,
   },
   {
     path: '**',
@@ -68,6 +50,5 @@ export const routes: Routes = [
       import('./Pages/wildcard/wildcard.component').then(
         (module) => module.WildcardComponent
       ),
-    pathMatch: 'full',
   },
 ];
